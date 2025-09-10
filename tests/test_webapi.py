726afc3271b92d211819dc73a7a4642a1395ed03
Johan Veenpere <johan.veenpere@gmail.com>
2025-09-10 21:07:34 +0300

    Added a unit test and changed how integration tests test the api.

--- tests/test_webapi.py
+++ tests/test_webapi.py
@@ -1,9 +1,24 @@
-from fastapi.testclient import TestClient
-from src.webapi import app
-
-client = TestClient(app)
+import requests
+from api import webapi
 
 
+# Integration test. Requires the containers to be up.
 def test_read_item():
-    response = client.get("/iris/1")
+    response = requests.get("http://localhost:8000/iris/1")
     assert response.status_code == 200
+
+
+# Integration test. Requires the containers to be up.
+def test_read_all():
+    response = requests.get("http://localhost:8000/irises")
+    assert response.status_code == 200
+
+
+# Unit test.
+def test_condition_parsing():
+    assert webapi.parse_condition("gt6.0") == ">6.0"
+    assert webapi.parse_condition("ge6.0") == ">=6.0"
+    assert webapi.parse_condition("eq6.0") == "=6.0"
+    assert webapi.parse_condition("lt6.0") == "<6.0"
+    assert webapi.parse_condition("le6.0") == "<=6.0"
+    assert webapi.parse_condition("ne6.0") == "!=6.0"
